06fa1f6b89c79be1f7d7cc535ef2d7f719978747
maxkram <max.kramer@live.com>
2022-07-21 15:28:52 -0400

    feat: add search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/create' element={<Create />} />
+                    <Route path='/search' element={<Search />} />
+                    <Route path='/recipes/:id' element={<Recipe />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
